9bd2a6cf2e7c7324590b7933077479c4ae703393
Ewa <zalewskaewa7@wp.pl>
2023-10-23 22:29:57 +0200

    finished frontend mentor project

--- src/components/Content.js
+++ src/components/Content.js
@@ -18,7 +18,10 @@ function Content() {
         : ""
       } 
 
-        <h1>Make <br />remote work</h1>
+       
+        
+          <h1>Make remote work</h1>
+      
         <p> Get your team in sync, no matter your location. Streamline processes, 
             create team rituals, and watch productivity soar.
         </p>
